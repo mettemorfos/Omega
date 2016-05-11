e917aab27bccaf1fd121e27e8a3c02237f48cb74
Mette Broegaard <mette_broegaard@Mettes-MacBook-Pro-2.local>
2016-05-11 12:21:30 +0200

    -

---
+++ Omega/OmegaViewController.swift
@@ -0,0 +1,36 @@
+//
+//  OmegaViewController.swift
+//  Omega
+//
+//  Created by Mette Broegaard on 11/05/16.
+//  Copyright © 2016 Inserve Technology AB. All rights reserved.
+//
+
+import UIKit
+
+class OmegaViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        print("I´m omega")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
