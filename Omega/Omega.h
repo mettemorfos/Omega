9711832120bd5c8388ac842f4917f54edeb5868f
Mette Broegaard <mette_broegaard@Mettes-MacBook-Pro-2.local>
2016-05-11 12:03:38 +0200

    Initial Commit

---
+++ Omega/Omega.h
@@ -0,0 +1,19 @@
+//
+//  Omega.h
+//  Omega
+//
+//  Created by Mette Broegaard on 11/05/16.
+//  Copyright © 2016 Inserve Technology AB. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Omega.
+FOUNDATION_EXPORT double OmegaVersionNumber;
+
+//! Project version string for Omega.
+FOUNDATION_EXPORT const unsigned char OmegaVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Omega/PublicHeader.h>
+
+
---
+++ Omega/OmegaMaker.swift
@@ -0,0 +1,18 @@
+//
+//  OmegaMaker.swift
+//  Omega
+//
+//  Created by Mette Broegaard on 11/05/16.
+//  Copyright © 2016 Inserve Technology AB. All rights reserved.
+//
+
+import Foundation
+
+
+class OmegaMaker {
+    
+    func makeBigO() {
+        print("Making a BIG OMEGA")
+    }
+    
+}
